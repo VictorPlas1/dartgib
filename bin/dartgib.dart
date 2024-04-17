9ab9740a6550141605c686e014c6708e3f1ca61d
Victor Plas <victor-real@hotmail.es>
2024-04-17 15:24:26 +0200

    primer commit

---
+++ bin/dartgib.dart
@@ -0,0 +1,5 @@
+import 'package:dartgib/dartgib.dart' as dartgib;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dartgib.calculate()}!');
+}
---
+++ test/dartgib_test.dart
@@ -0,0 +1,8 @@
+import 'package:dartgib/dartgib.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
